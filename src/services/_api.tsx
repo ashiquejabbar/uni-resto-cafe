dd318146883614f82c381237ac106aa736e1ef3c
Ashique PJ <ashiquejabbar007@gmail.com>
2024-12-12 16:23:51 +0530

    env config added

--- src/services/_api.tsx
+++ src/services/_api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Restaurant } from '../types/menu';
 
-const API_URL = 'https://run.mocky.io/v3/180b4edd-7baa-4e3b-aac2-4746df1ac904';
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const fetchMenuData = async (): Promise<Restaurant> => {
   const response = await axios.get(API_URL);
